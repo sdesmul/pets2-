717d52af19a9d5de96cc24eb6f01ef31098f76e6
Kat Truitt <ktruitt@mail.greenriver.edu>
2019-04-22 17:47:06 -0400

    Resovled merge conflict

--- index.php
+++ index.php
@@ -58,23 +58,31 @@ $f3->route('GET /@animalType', function($f3, $params){
 //define a route for order
 $f3->route('GET /order', function(){
 
-    $_SESSION['animal'] = $_POST['animal'];
     $view= new Template();
     echo $view->render('views/form1.html');
+});
+
+$f3->route('POST /order1', function(){
+    $_SESSION['animal'] = $_POST['animal'];
+
+
+    $view= new Template();
+    echo $view->render('views/form2.html');
 });
 
 $f3->route('POST /order2', function(){
     $_SESSION['color'] = $_POST['color'];
 
     $view= new Template();
-    echo $view->render('views/form2.html');
-});
-
-$f3->route('POST /results', function(){
-
-    $view= new Template();
     echo $view->render('views/results.html');
 });
+ $f3->route('GET /results', function()
+ {
+     $_SESSION['animal'] = $_POST['animal'];
+     $_SESSION['color'] = $_POST['color'];
+     $view= new Template();
+     echo $view->render('views/results.html');
+ });
 
 
 //run Fat-free
